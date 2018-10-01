0a5889f8966e711d99e078cf0c6d7bac715cbabb
Gustavo Sarturi <34655704+gustavosarturi@users.noreply.github.com>
2018-10-01 19:26:04 -0300

    Update integral.jl

--- integral.jl
+++ integral.jl
@@ -1,9 +1,9 @@
-function simpsonrep(f, a, b; m = 101)
+function simpsonrep(f::Function, a::Real, b::Real; m = 101)
     m % 2 == 0 && (m += 1) #If
     h = (b - a) / (m - 1)
     v = zeros(m) #Cria vetor coluna de m posições;
     for i = 1:m
-        v[i] = a + h * (i-1)
+        v[i] = a + h * (i - 1)
     end
     Σ1, ⁠⁠Σ2 = (0, 0)
     for i = 2: 2: m-1
@@ -14,23 +14,23 @@ function simpsonrep(f, a, b; m = 101)
     return (h / 3) * (f(v[1]) + 4 * Σ1 + 2 * Σ2 + f(v[m]))
 end
 
-function simpsoneps(f, a, b, ϵ; M = 1.0) #calculo do novo ϵ
+function simpsoneps(f::Function, a::Real, b::Real, ϵ; M = 1.0) #calculo do novo ϵ
     h = ((180 * ϵ) / ((b - a) * M))^(0.25)
     m = ceil(Int, (b - a)/h + 1) #ceil arredonda para +, Ex.: ceil(π) = 4.0
     return simpsonrep(f, a, b, m)
 end
 
-function simpson(f, a, b)
+function simpson(f::Function, a::Real, b::Real)
     μ = (b - a) / 6
     return μ * (f(a) + 4 * f((a + b) * 0.5) + f(b))
 end
 
-function simpson_adaptivo(f, a, b, ϵ)
+function simpson_adaptivo(f::Function, a::Real, b::Real, ϵ::Real)
     ∫ = simpson(f, a, b)
     return simpson_adaptivo_recursivo(f, a, b, ϵ, ∫)
 end
 
-function simpson_adaptivo_recursivo(f, a, b, ϵ, I)
+function simpson_adaptivo_recursivo(f::Function, a::Real, b::Real, ϵ::Real, I::Real)
     m = (a + b) * 0.5
     R = simpson(f, a, b)
     L = simpson(f, a, c)
